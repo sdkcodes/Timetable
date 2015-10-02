5526639a811efdae81b9e48f5c3958b1dfa8c2c9
R. David Dunphy <rdd.dunphy@gmail.com>
2015-10-02 21:12:08 +0100

    Add time fields to TimetableSlot.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 
 		Timetable t = new Timetable("myTimetable");
-		t.addSlot(new TimetableSlot("slot1"));
-		t.addSlot(new TimetableSlot("slot2"));
+		t.addSlot(new TimetableSlot(0, "slot1", DayOfWeek.FRIDAY, LocalTime.of(10, 30), Duration.ofHours(1)));
+		t.addSlot(new TimetableSlot(1, "slot2", DayOfWeek.TUESDAY, LocalTime.of(12, 0), Duration.ofMinutes(150)));
 		System.out.println(t.toString());
 
 	}
--- src/TimetableSlot.java
+++ src/TimetableSlot.java
@@ -1,14 +1,100 @@
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 
 public class TimetableSlot {
 	
+	private int id;
 	private String name;
+	private DayOfWeek day;
+	private LocalTime start;
+	private Duration duration;
 	
-	public TimetableSlot(String name) {
+	public TimetableSlot(int id, String name, DayOfWeek day, LocalTime start, Duration duration) {
+		this.id = id;
 		this.name = name;
+		this.setDay(day);
+		this.start = start;
+		this.setDuration(duration);
 	}
 
 	public String toString() {
+		String str = name + " (" + id + "): ";
+		str += day.getDisplayName(TextStyle.SHORT, Locale.ENGLISH) + " ";
+		str += start.toString() + "-" + getFinish();
+		return str;
+	}
+	
+	/**
+	 * @return the finish time, as calculated from start and duration
+	 */
+	public LocalTime getFinish() {
+		return start.plus(duration);
+	}
+
+	/**
+	 * @return the id number
+	 */
+	public int getID() {
+		return id;
+	}
+	
+	/**
+	 * @return the name
+	 */
+	public String getName() {
 		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * @return the start
+	 */
+	public LocalTime getStart() {
+		return start;
+	}
+
+	/**
+	 * @param start the start to set
+	 */
+	public void setStart(LocalTime start) {
+		this.start = start;
+	}
+
+	/**
+	 * @return the duration
+	 */
+	public Duration getDuration() {
+		return duration;
+	}
+
+	/**
+	 * @param duration the duration to set
+	 */
+	public void setDuration(Duration duration) {
+		this.duration = duration;
+	}
+
+	/**
+	 * @return the day
+	 */
+	public DayOfWeek getDay() {
+		return day;
+	}
+
+	/**
+	 * @param day the day to set
+	 */
+	public void setDay(DayOfWeek day) {
+		this.day = day;
 	}
 }
